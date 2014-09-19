8dfc759c2e47a99e9637d72fda0475874835ea6a
Jonathan Martini <jon@isadminsmacbook.rp.corp>
2014-09-19 10:40:57 -0400

    Challenge 1 build - Added functionality for the name field

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -10,10 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
-    
     @IBOutlet weak var mailButton: UIButton!
     
     override func viewDidLoad() {
@@ -28,13 +28,21 @@ class ViewController: UIViewController {
 
     @IBAction func sendMailButtonPressed(sender: UIButton) {
         // Code will evaluate when we press the button
-        // adding a comment here to test commits
+        
+        //This code performs functions on the nameLabel
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        enterNameTextField.text = ""
+        
+        //This code performs functions on the messageLabel
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
         messageLabel.textColor = UIColor.redColor()
         mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+        
     }
 }
 
